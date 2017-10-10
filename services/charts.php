b2cf72a22025744a990b1007311bbf90299fe930
George Keremidchiev <gmkeremidchiev@gmail.com>
2017-10-10 15:41:21 +0200

    Point 3.3.

--- services/charts.php
+++ services/charts.php
@@ -5,23 +5,34 @@
     $result = null; 
     if(isset($_REQUEST['cmd']) && $_REQUEST['cmd'] == 'load' && isset($_REQUEST['source']))
     {
+        $from_date = null;
+        $to_date = null;
+        if(isset($_REQUEST['from-date']) && isset($_REQUEST['to-date'])) 
+        {
+            if(validateDate($_REQUEST['from-date']) && validateDate($_REQUEST['to-date'])) 
+            {
+                $from_date = $_REQUEST['from-date'];
+                $to_date = $_REQUEST['to-date'];
+            }
+        } 
+
 		switch($_REQUEST['source']) {
 			case 'users':
 					$usersId = null;
 					if(isset($_REQUEST['users-id'])) {
 						$usersId = (int)$_REQUEST['users-id'];
 					}
-                    loadUsers($usersId); 
+                    loadUsers($usersId, $from_date, $to_date); 
 				break;
 			case 'projects':
-                    loadProjects();
+                    loadProjects($from_date, $to_date);
 				break;
 			default:
 				print json_encode(['status' => 'error']);
 		}
 	}
 
-    function loadUsers($usersId)
+    function loadUsers($usersId, $from_date, $to_date)
     {
         global $db_conn;
 
@@ -35,12 +46,17 @@
 			array_push($response, [ $row->first_name." ".$row->last_name." ".$row->email, (real)$row->sum_hours, "red" ]);
 		}
 
+        $date_part = '';
+        if($from_date !== null && $to_date !== null)
+        {
+            $date_part = " where date >= '".pg_escape_string($from_date)."' and date <= '".pg_escape_string($to_date)."'";
+        }
+
         $result = @pg_query($db_conn, "select B.first_name, B.last_name, B.email, A.sum_hours
         from 
-        (select sum(hours) as sum_hours, users_id from time_logs A group by A.users_id order by sum_hours desc limit 10) A
+        (select sum(hours) as sum_hours, users_id from time_logs $date_part group by users_id order by sum_hours desc limit 10) A
         join users B on A.users_id = B.id
 		order by A.sum_hours desc");
-
         while($row = pg_fetch_object($result))
         {
             array_push($response, [ $row->first_name." ".$row->last_name." ".$row->email, (real)$row->sum_hours, "blue" ]);
@@ -49,12 +65,19 @@
 		print json_encode(['status' => 'ok', 'title' => 'Users', 'payload' => $response]);
     }
 
-	function loadProjects()
+	function loadProjects($from_date, $to_date)
     {
         global $db_conn;
+
+       	$date_part = '';
+        if($from_date !== null && $to_date !== null)
+        {
+            $date_part = " where date >= '".pg_escape_string($from_date)."' and date <= '".pg_escape_string($to_date)."'";
+        } 
+
         $result = @pg_query($db_conn, "select B.name, A.sum_hours
 		from
-		(select sum(hours) as sum_hours, projects_id from time_logs group by projects_id order by sum_hours desc limit 10) A
+		(select sum(hours) as sum_hours, projects_id from time_logs $date_part group by projects_id order by sum_hours desc limit 10) A
 		join projects B on A.projects_id = B.id 
 		order by A.sum_hours desc");
 
@@ -65,5 +88,27 @@
         }
 
         print json_encode(['status' => 'ok', 'title' => 'Projects', 'payload' => $response]);
+    }
+
+    //mm/dd/yyyy
+    function validateDate($inputDate) 
+    {
+        if(!isset($inputDate)) 
+        {
+            return false;    
+        }
+
+        if(strlen($inputDate) != 10)  
+        {
+            return false;
+        }
+
+        $parts = explode("/", $inputDate);
+        if(count($parts) != 3) 
+        {
+            return false;
+        }
+
+        return checkdate($parts[0], $parts[1], $parts[2]);
     }
 ?>
--- www/index.php
+++ www/index.php
@@ -12,10 +12,11 @@
 	<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 
     <link href="libs/jquery-loader/jquery.loader.css" rel="stylesheet">
+    <link rel="stylesheet" href="libs/jquery-ui/jquery-ui.min.css">
 
     <script src="libs/jquery/jquery-1.11.2.min.js"></script>
     <script src="libs/jquery-loader/jquery.loader.js"></script>
-
+    <script src="libs/jquery-ui/jquery-ui.min.js"></script>
 
 	<script type="text/javascript">
 		var currUserPage = 0;
@@ -128,9 +129,6 @@
                         break;
                 }
 		}
-	</script>
-
-	<script type="text/javascript">
 
     	// Load the Visualization API and the corechart package.
       	google.charts.load('current', {'packages':['corechart']});
@@ -147,7 +145,7 @@
 			loadData('users', usersId);
 		}
 
-		function loadData(source, usersId) {
+		function loadData(source, usersId, fromDate, toDate) {
 			$.ajax( 
                     {   
                         url: "<?php echo(WEB_ROOT); ?>charts.php",
@@ -158,7 +156,9 @@
                         {
                             "cmd": "load",
 							"source": source,
-							"users-id": usersId
+							"users-id": usersId,
+							"from-date": fromDate,
+							"to-date": toDate
                         },
                         complete: function(result)
                         {   
@@ -175,19 +175,17 @@
 		}
 
       	function drawChart(title, payload) {
-
-	        // Create the data table.
-/*
-    	    var data = new google.visualization.DataTable();
-	        data.addColumn('string', 'Topping');
-	        data.addColumn('number', 'Hours');
-			//data.addColumn({'string', role:'color'});
-console.log(payload);
-			data.addRows(payload);
-*/
-
-	payload.unshift(['Element', 'Hours', { role: 'style' }]);
-	var data = new google.visualization.arrayToDataTable(payload);
+			if(payload.length === 0) {
+				$("#chart-div").hide();
+				$("#data-source-char").hide();
+				return;
+			} else {
+				$("#chart-div").show();
+                $("#data-source-char").show();
+			}	
+			// Create the data table.
+			payload.unshift(['Element', 'Hours', { role: 'style' }]);
+			var data = new google.visualization.arrayToDataTable(payload);
     	    // Set chart options
  	    	var options = {'title':title,
             	'width':500,
@@ -196,49 +194,101 @@ console.log(payload);
         	// Instantiate and draw our chart, passing in some options.
 	        var chart = new google.visualization.BarChart(document.getElementById('chart-div'));
     	    chart.draw(data, options);
-      }
-    </script>
+		}
+
+    //Datapicker
+	$( document ).ready(function() {
+		$( "#from" ).val('');
+		$( "#to" ).val('');
+		var dateFormat = "mm/dd/yy",
+      	from = $( "#from" )
+        	.datepicker({
+          		defaultDate: "+1w",
+          		changeMonth: true,
+          		numberOfMonths: 1
+        	})
+        .on( "change", function() {
+        	to.datepicker( "option", "minDate", getDate( this ) );
+        }),
+      	to = $( "#to" ).datepicker({
+        	defaultDate: "+1w",
+        	changeMonth: true,
+        	numberOfMonths: 1
+      	})
+      	.on( "change", function() {
+        	from.datepicker( "option", "maxDate", getDate( this ) );
+      	});
+ 
+    	function getDate( element ) {
+		    var date;
+      		try {
+		        date = $.datepicker.parseDate( dateFormat, element.value );
+		      } catch( error ) {
+		        date = null;
+      		}
+
+      		return date;
+    	}
+
+		$('#apply-date').click(function() {
+			loadData($('input[type=radio][name=chart-data-source]:checked').val(), null, 
+						$( "#from" ).val(), $( "#to" ).val())		
+		});	
+	});
+   </script>
 </head>
 <body>
-    <div class="col-xs-6">
-    	<table id="users-table" class="table table-hover">
-			<thead>
-      			<tr>
-			        <th>First Name</th>
-        			<th>Last Name</th>
-        			<th><a id="order-by-email-users" href="#" style="text-decoration: none;">Email <span id="order-by-email-arrow-users">&#8595;</span></a></th>
-      			</tr>
-    		</thead>
-		    <tbody>
-			</tbody>
-  		</table>
-        <div>
-		    <div class="col-xs-6"><a id="prev-users-page" href="#" class="btn btn-primary">&#8612; Назад</a></div>
-		    <div class="col-xs-6"><a id="next-users-page" href="#" class="btn btn-primary">Напред &#x21A6;</a></div>
-        </div>
-        <div class="jumbotron" style="background-color: #FFF">
-		        <a href="#" id="init-db-reload-page" class="btn btn-danger btn-block">Инициализация на базата и презарежда страницата</a> 
-        </div>
-    </div>
-    <div class="col-xs-6">
-        <div id="chart-div"></div>
-		<div class="form-group">
-      		<label class="col-lg-2 control-label">Data source</label>
-      		<div class="col-lg-10">
-        		<div class="radio">
-		          <label>
-		            <input type="radio" name="chart-data-source" id="optionsRadios1" value="users" checked="">
-					Users
-		          </label>
-        		</div>
-		        <div class="radio">
-		          <label>
-		            <input type="radio" name="chart-data-source" id="optionsRadios2" value="projects">
-		            Projects
-		          </label>
-		        </div>
-	      	</div>
+	<div class="col-sm-12">
+		<div class="col-xs-6"></div>
+		<div class="col-xs-6">
+			<label for="from">From</label>
+			<input type="text" id="from" name="from">
+			<label for="to">to</label>
+			<input type="text" id="to" name="to">
+			<a href="#" id="apply-date" class="btn btn-success btn btn-sm">Apply</a>
+		</div>
+	</div>
+	<div class="col-sm-12">
+	    <div class="col-xs-6">
+    		<table id="users-table" class="table table-hover">
+				<thead>
+      				<tr>
+			    	    <th>First Name</th>
+	        			<th>Last Name</th>
+    	    			<th><a id="order-by-email-users" href="#" style="text-decoration: none;">Email <span id="order-by-email-arrow-users">&#8595;</span></a></th>
+      				</tr>
+    			</thead>
+			    <tbody>
+				</tbody>
+  			</table>
+	        <div>
+			    <div class="col-xs-6"><a id="prev-users-page" href="#" class="btn btn-primary">&#8612; Назад</a></div>
+			    <div class="col-xs-6"><a id="next-users-page" href="#" class="btn btn-primary">Напред &#x21A6;</a></div>
+	        </div>
+    	    <div class="jumbotron" style="background-color: #FFF">
+			        <a href="#" id="init-db-reload-page" class="btn btn-danger btn-block">Инициализация на базата и презарежда страницата</a> 
+	        </div>
     	</div>
-    </div>
+	    <div class="col-xs-6">
+    	    <div id="chart-div"></div>
+			<div id="data-source-char" class="form-group">
+	      		<label class="col-lg-2 control-label">Data source</label>
+    	  		<div class="col-lg-10">
+        			<div class="radio">
+		    	      <label>
+		        	    <input type="radio" name="chart-data-source" value="users" checked="">
+						Users
+			          </label>
+    	    		</div>
+			        <div class="radio">
+		    	      <label>
+		        	    <input type="radio" name="chart-data-source" value="projects">
+		            	Projects
+			          </label>
+			        </div>
+	    	  	</div>
+    		</div>
+    	</div>
+	</div>
 </body>
 </html>
